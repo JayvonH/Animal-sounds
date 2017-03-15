adce65cbf4c8495d7e8d69ca0b03f169de231cf7
JayvonH <Jayvon_harris97@hotmail.com>
2017-03-15 11:37:56 -0400

    delete animal_code.rb

--- animal_code.rb
+++
@@ -1,8 +0,0 @@
-require "minitest/autorun"
-require_relative "cat.rb"
-
-class TestClassSound < Minitest::Test
-	def test_cat_meow
-		animal = Cat.new("Kitty")
-		assert_equal("Kitty", animal.name)
-end
