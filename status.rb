34e8999e01d1767003d30d59ace22524db595cf2
JayvonH <jayvon_harris97@hotmail.com>
2017-03-15 12:46:19 -0400

    the animals are still alive

--- status.rb
+++ status.rb
@@ -3,4 +3,6 @@ require_relative "dog.rb"
 require_relative "piggy.rb"
 
 animal = Dog.new("Fido")
-p animal.info
+animal1 = Cat.new("Kitty")
+animal2 = Pig.new("Poogie")
+p animal.info+animal1.info+animal2.info
